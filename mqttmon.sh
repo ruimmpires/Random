574c719a23a529441c81a308eb75f879f64e44db
Rui Pires <46453695+ruimmpires@users.noreply.github.com>
2021-10-03 18:35:26 +0100

    Update mqttmon.sh

--- mqttmon.sh
+++ mqttmon.sh
@@ -6,38 +6,41 @@ mosquitto_sub -t 'home/temp/1/tx' > /var/www/html/outtemp1.csv &
 echo mosquitto subscribed to home/watertemp/1/tx 
 mosquitto_sub -t 'home/watertemp/1/tx' > /var/www/html/watertemp1.csv &
 
-echo press q to stop
-sleep 15
-while true
-do
-	#hum=$(cat /var/www/html/hum1.csv)
-	hum=$(tail -1 /var/www/html/hum1.csv)
-	hum=${hum:0:2}
-	timestamp=$(date +'20%y/%m/%d-%T')
-	#timestamp2=$(date +'%y/%m/%d-%T')
-	printf "$timestamp","$hum\n"
-	echo "$timestamp,$hum" >> /var/www/html/hum.csv
-	
-	#temp=$(cat /var/www/html/outtemp1.csv)
-	temp=$(tail -1 /var/www/html/outtemp1.csv)
-	temp=${temp:0:2}
-	printf "$timestamp","$temp\n"
-	echo "$timestamp,$temp" >> /var/www/html/outtemp.csv
-	
-	#wtemp=$(cat /var/www/html/watertemp1.csv)
-	wtemp=$(tail -1 /var/www/html/watertemp1.csv)
-	wtemp=${wtemp:0:4}
-	printf "$timestamp","$wtemp\n"
-	echo "$timestamp,$wtemp" >> /var/www/html/watertemp.csv
-	
-	echo "$timestamp,$temp,$hum,$wtemp" >> /var/www/html/mqttstats.csv
-		
-	if [[ $input = "q" ]] || [[ $input = "Q" ]]; then
-        echo end of script
-        kill -9 $(pidof mosquitto_sub)
-        break 
-    fi
-    
-    sleep 15m
-done
+hum=$(tail -1 /var/www/html/hum1.csv)
+hum=${hum:0:2}
+timestamp=$(date +'20%y/%m/%d-%T')
 
+printf "$timestamp","$hum\n"
+echo "$timestamp,$hum" >> /var/www/html/hum.csv
+
+
+temp=$(tail -1 /var/www/html/outtemp1.csv)
+#the below command failed when the temperature was below 10
+#(( temp=$temp+0 ))
+if [ $((temp)) -lt 10 ]
+then
+        temp=${temp:2:3}
+        #temp=${temp:0:2}
+        echo $temp
+fi
+printf "$timestamp","$temp\n"
+echo "$timestamp,$temp" >> /var/www/html/outtemp.csv
+
+
+wtemp=$(tail -1 /var/www/html/watertemp1.csv)
+wtemp=${wtemp:0:4}
+printf "$timestamp","$wtemp\n"
+echo "$timestamp,$wtemp" >> /var/www/html/watertemp.csv
+
+power=$(tail -1 /var/www/html/power1.csv)
+printf "$timestamp","$power\n"
+echo "$timestamp,$power" >> /var/www/html/power.csv
+
+potential=$(tail -1 /var/www/html/potential1.csv)
+printf "$timestamp","$potential\n"
+echo "$timestamp,$potential" >> /var/www/html/potential.csv
+
+echo "$timestamp,$temp,$hum,$wtemp,$power,$potential" >> /var/www/html/mqttstats.csv
+
+
+kill -9 $(pidof mosquitto_sub)
